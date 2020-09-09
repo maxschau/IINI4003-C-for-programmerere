a8eba12e26da73bf8b24185b70c3813077348590
Max Torre Schau <max.torre.schau@gmail.com>
2020-09-09 10:56:58 +0200

    Finished oving5

---
+++ Oving5/cmake-build-debug/CMakeFiles/Oving5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oving5.dir/main.cpp.o"
+  "Oving5"
+  "Oving5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oving5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
